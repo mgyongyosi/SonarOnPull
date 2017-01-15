d9c8c6fd71e4240f4d82ad52942bc74599151802
mgyongyosi <mgyongyosi@users.noreply.github.com>
2017-01-15 14:29:30 +0100

    AddNumbers, AddAndSubstractNumbers added

--- SonarOnPull/Program.cs
+++ SonarOnPull/Program.cs
@@ -10,6 +10,32 @@ namespace SonarOnPull
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Result:" + AddNumbers(3,4,52,3));
+            Console.WriteLine("Result:" + AddAndSubstractNumbers(3, 10, 52,55));
+        }
+
+        static int AddNumbers(params int[] n)
+        {
+            int result = 0;
+            foreach(int num in n)
+            {
+                result += num;
+            }
+            return result;
+        }
+
+        static int AddAndSubstractNumbers(params int[] nums)
+        {
+            int result = 0;
+            foreach(int num in nums)
+            {
+                result += num;
+            }
+            foreach(int num in nums)
+            {
+                result -= (num - 2);
+            }
+            return result;
         }
     }
 }
